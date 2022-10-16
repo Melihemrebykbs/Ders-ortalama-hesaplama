b73614320b5654f53dbe8bb35cfaf4ef6d498782
Melihemrebykbs <buyukbasmelih6@gmail.com>
2022-10-16 19:31:44 +0300

    Ders not hesaplama programı
    Ders not hesaplama programı

---
+++ Ders_not_hesaplama/ders_not_hesaplama.py
@@ -0,0 +1,32 @@
+"""print("      Notlar      ")
+
+not1 = input("1. Not:")
+not2 = input("2. Not:")
+soz1 = input("Sözlü Notu:")
+
+sonuc = not1+not2+soz1
+
+print(not1, "+" , not2, "+", soz1 ,"=" ,sonuc)"""
+
+print("      Notlar      ")
+
+not1 = input("1. Not:")
+not2 = input("2. Not:")
+soz1 = input("Sözlü Notu:")
+
+tümdersnotsayısı = ["not1","not2","soz1"]
+
+len(tümdersnotsayısı)
+
+dersnotsayısı=len(tümdersnotsayısı)
+
+sonuc = int(not1)+int(not2)+int(soz1)
+
+ortalama = sonuc / dersnotsayısı
+
+print("Ortalama: ", ortalama)
+
+
+
+
+
